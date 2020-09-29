13cbd8300425fda19c762b1b14e40e00d74e00ff
Andrew <andrewlei323@gmail.com>
2020-09-29 11:56:32 +1000

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+/**
+* Template Name: MyPortfolio - v2.1.0
+* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function($) {
+  "use strict";
+
+  var burgerMenu = function() {
+    $('.burger').click(function(e) {
+      $(window).scrollTop(0);
+      if (!$('.burger').hasClass('active'))
+        $('.burger').addClass('active');
+      else
+        $('.burger').removeClass('active');
+    });
+  }
+  burgerMenu();
+
+  var siteIstotope = function() {
+    var $container = $('#portfolio-grid').isotope({
+      itemSelector: '.item',
+      isFitWidth: true
+    });
+
+    $(window).resize(function() {
+      $container.isotope({
+        columnWidth: '.col-sm-3'
+      });
+    });
+
+    $container.isotope({
+      filter: '*'
+    });
+
+    $('#filters').on('click', 'a', function(e) {
+      e.preventDefault();
+      var filterValue = $(this).attr('data-filter');
+      $container.isotope({
+        filter: filterValue
+      });
+      $('#filters a').removeClass('active');
+      $(this).addClass('active');
+    });
+  }
+  $(window).on('load', function() {
+    siteIstotope();
+  });
+
+  var siteOwlCarousel = function() {
+    $('.testimonial-carousel').owlCarousel({
+      center: true,
+      items: 1,
+      loop: true,
+      margin: 0,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+  };
+  siteOwlCarousel();
+
+  $(window).on('load', function() {
+    AOS.init({
+      easing: 'ease',
+      duration: 1000,
+      once: true
+    });
+  });
+
+})(jQuery);
+
+function sendEmail() {
+  Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "rulesilol@gmail.com",
+      Password : "98C6627F5D649EAD74049A45C822C03AEAC6",
+      To : 'andrewlei323@gmail.com',
+      From : "account@gmail.com",
+      Subject : "Test email",
+      Body : "This is a test"
+  }).then(
+    message => alert(message)
+  );
+  }
